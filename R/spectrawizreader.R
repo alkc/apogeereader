0e538e56ca8ff0f337a4a1b7e764ba4ee1d3b5b7
Alexander Koc <alexander.koc@slu.se>
2018-07-28 12:03:54 +0200

    Add spectrawizreader.R and start typing up a description

---
+++ R/spectrawizreader.R
@@ -0,0 +1,10 @@
+#' @title Simple loading of SpectraWiz output files in R.
+#'
+#' @description
+#'
+#' Use the \code{read_spectrawiz()} to load SpectraWiz output files into wide format data frames in R.
+#'
+#' @docType package
+#' @author Alexander Koc
+#' @name spectrawizreader
+NULL
