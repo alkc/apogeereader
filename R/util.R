cda4a54751a93fcb5e45758025b0255456e57f7a
Alexander Koc <alexander.koc@slu.se>
2018-07-28 12:02:43 +0200

    Less silly error message if missing file

--- R/util.R
+++ R/util.R
@@ -3,8 +3,9 @@
 stop_if_files_are_missing <- function(file) {
   files_that_do_not_exist <- !file.exists(file)
   if(isTRUE(sum(files_that_do_not_exist) > 0)) {
+    # TODO: Figure out something better instead of just printing first missing file
     first_nonexistent_file <- file[files_that_do_not_exist][1]
-    error_message <- paste("Hey now, the following file does not exist:",
+    error_message <- paste("No file exists at the specified path:",
                            first_nonexistent_file)
     stop(error_message)
   }
