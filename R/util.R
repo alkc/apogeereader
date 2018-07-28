191a68bd567806d8ab6f007aa8005a495e33f710
Alexander Koc <alexander.koc@slu.se>
2018-07-28 11:43:15 +0200

    Rename function for missing file check and fix bug where program stops even if no file is missing

--- R/util.R
+++ R/util.R
@@ -1,8 +1,8 @@
 # In case user supplies one or more non-existent paths to read_spectrawiz
 # better to just print one file in err msg, rather than all of them if many
-check_if_files_are_there <- function(file) {
+stop_if_files_are_missing <- function(file) {
   files_that_do_not_exist <- !file.exists(file)
-  if(isTRUE(length(files_that_do_not_exist) > 0)) {
+  if(isTRUE(sum(files_that_do_not_exist) > 0)) {
     first_nonexistent_file <- file[files_that_do_not_exist][1]
     error_message <- paste("Hey now, the following file does not exist:",
                            first_nonexistent_file)
