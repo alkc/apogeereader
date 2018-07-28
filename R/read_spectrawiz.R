32c5731c66b7b345f77dfe5a282564bcca424f63
Alexander Koc <alexander.koc@slu.se>
2018-07-28 11:43:40 +0200

    Made minor adjustments and added comments

--- R/read_spectrawiz.R
+++ R/read_spectrawiz.R
@@ -1,27 +1,33 @@
 # Function for processing a single spectrawiz file
 process_file <- function(file) {
-  file_contents <- scan(file, what = character(), sep = "\n", quiet = TRUE,
-                        comment.char = "\"")
-  # Remove whitespace
+  file_contents <- scan(file, what = character(), sep = "\n",
+                        quiet = TRUE, comment.char = "\"")
+
+  # Remove whitespace from all lines:
   file_contents <- trimws(file_contents)
 
   # Split by a single space and remove any empty columns per row
-  # TODO: Investigate if this is needed or if spectrawiz *always* uses the
-  # TODO: same column delim spacing, if yes, adjust split arg accordingly and skip
+  # TODO: Investigate if this is needed or if spectrawiz *always* uses the [...]
+  # ... same column delim spacing, if yes, adjust split arg accordingly and skip
   file_contents <- strsplit(file_contents, split = " ")
   file_contents <- lapply(file_contents, function(entry) entry[!entry == ""])
 
-  # Combine all wavelength rows into a matrix, transpose and convert into
-  # a data frame
+  # Combine all wavelength rows into a matrix, transpose into wide form and
+  # convert into a data frame
   file_contents <- do.call(rbind, file_contents)
+
+  # Wavelengths are converted to char to be used as column names
   wavelengths <- as.character(file_contents[,1])
+
   spectral_data <- t(as.double(file_contents[,2]))
   colnames(spectral_data) <- wavelengths
   spectral_data <- as.data.frame(spectral_data)
+
   # Add filename of loaded file as a column
-  # TODO: Make this optional
+  # TODO: Make filename column optional?
   spectral_data <- cbind(data.frame(filename=file,stringsAsFactors = FALSE),
                          spectral_data)
+
   spectral_data
 }
 
@@ -29,7 +35,7 @@ process_file <- function(file) {
 read_spectrawiz <- function(file) {
   multiple_files <- length(file) > 1
   # Yell at user if one or more files defined in file don't exist
-  check_if_files_are_there(file)
+  stop_if_files_are_missing(file)
 
   if(isTRUE(multiple_files)) {
     spectral_data <- lapply(file, process_file)
