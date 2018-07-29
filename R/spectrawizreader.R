a736106a0064f1e414870d2c1e14e28f79745a8b
Alexander Koc <alexander.koc@slu.se>
2018-07-29 11:04:27 +0200

    Move .trm_file() into spectrawizreader.R

--- R/spectrawizreader.R
+++ R/spectrawizreader.R
@@ -11,6 +11,28 @@
 #' @name spectrawizreader
 NULL
 
+#' @name read_spectrawiz
+#' @title Read one or more SpectraWiz output files into a dataframe
+#' @description \code{read_spectrawiz()} takes a single .TRM, .IRR, .ABS, and
+#' .SSM file or a vector of file paths as input and outputs a \code{data.frame}
+#' containing the spectra and the file paths of the input files.
+#' @export
+#' @author Alexander Koc
+#' @param file a vector of paths to SpectraWiz file(s)
+#' @return a \code{data.frame} of filenames and spectra associated with the input files in \code{file}
+#' @examples
+#'
+#' # Get the path to the demo file
+#'
+#' path_to_spectrawiz_file <- .trm_file()
+#'
+#' # Example with one file name
+#'
+#' spectral_data <- read_spectrawiz(path_to_spectrawiz_file)
+#'
+#' # Print the five first columns:
+#' print(spectral_data[1,1:5])
+#'
 #' @export
 read_spectrawiz <- function(file) {
   multiple_files <- length(file) > 1
@@ -25,3 +47,19 @@ read_spectrawiz <- function(file) {
   }
   spectral_data
 }
+
+#' @name .trm_file
+#' @title Get file path of a sample SpectraWiz .TRM file
+#' @description This function returns a path to a sample file that you can use
+#' to test the \code{read_spectrawiz()} function.
+#' @author Alexander Koc
+#' @return a character vector containing a file path to a sample file
+#' @examples
+#'
+#' path_to_spectrawiz_file <- .trm_file()
+#' path_to_spectrawiz_file
+#' @export
+.trm_file <- function() {
+  system.file("extdata", "wheat.TRM", package = "spectrawizreader")
+}
+
--- R/trm-file.R
+++
@@ -1,4 +0,0 @@
-#' @export
-.trm_file <- function() {
-    system.file("extdata", "wheat.TRM", package = "spectrawizreader")
-}
