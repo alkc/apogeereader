b4e266178f0d23c30001391d06721a26d9ca0c14
Alexander Koc <alexander.koc@slu.se>
2019-06-10 15:15:02 +0200

    Fix typo

--- R/read-spectrawiz.R
+++ R/read-spectrawiz.R
@@ -4,8 +4,9 @@
 #'
 #' @description
 #'
-#' Use the \code{read_spectrawiz()} to load SpectraWiz output files into wide
-#' format data frames in R.
+#' Use the \code{read_spectrawiz()} function to load SpectraWiz output files into wide
+#' format data frames in R, or the  \code{read_spectrovision()} function for
+#' SpectroVision .csv outpur-files.
 #'
 #' @docType package
 #' @author Alexander Koc
