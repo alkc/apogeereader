13b517b6c84df8522781210bbbcbaf5c36de883c
Alexander Koc <alexander.koc@slu.se>
2019-06-11 14:43:56 +0200

    Move package help page to own file

---
+++ R/apogeereader.R
@@ -0,0 +1,12 @@
+#' @title Simple loading of SpectraWiz output files in R.
+#'
+#' @description
+#'
+#' Use the \code{read_spectrawiz()} function to load SpectraWiz output files into wide
+#' format data frames in R, or the  \code{read_spectrovision()} function for
+#' SpectroVision .csv outpur-files.
+#'
+#' @docType package
+#' @author Alexander Koc
+#' @name spectrawizreader
+NULL
--- R/read-spectrawiz.R
+++ R/read-spectrawiz.R
@@ -1,18 +1,5 @@
 #' @include process-spectra.R
 
-#' @title Simple loading of SpectraWiz output files in R.
-#'
-#' @description
-#'
-#' Use the \code{read_spectrawiz()} function to load SpectraWiz output files into wide
-#' format data frames in R, or the  \code{read_spectrovision()} function for
-#' SpectroVision .csv outpur-files.
-#'
-#' @docType package
-#' @author Alexander Koc
-#' @name spectrawizreader
-NULL
-
 #' @name read_spectrawiz
 #' @title Read one or more SpectraWiz output files into a dataframe
 #' @description \code{read_spectrawiz()} takes a single .TRM, .IRR, .ABS, and
