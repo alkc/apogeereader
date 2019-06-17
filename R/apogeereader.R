df97721a5ef94534b92538b0c872de916f1127a2
Alexander Koc <alexander.koc@slu.se>
2019-06-17 11:58:05 +0200

    Update package documentation to reflect new name

--- R/apogeereader.R
+++ R/apogeereader.R
@@ -2,11 +2,11 @@
 #'
 #' @description
 #'
-#' Use the \code{read_spectrawiz()} function to load SpectraWiz output files into wide
-#' format data frames in R, or the  \code{read_spectrovision()} function for
-#' SpectroVision .csv outpur-files.
+#' Use the \code{read_spectrawiz()} function to load SpectraWiz output files into
+#' wide format data frames in R, or the \code{read_spectrovision()} function for
+#' SpectroVision .csv output-files.
 #'
 #' @docType package
 #' @author Alexander Koc
-#' @name spectrawizreader
+#' @name apogeereader
 NULL
