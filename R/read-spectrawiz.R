30c037cceed8e24de339157f7c242569efd16cdd
Alexander Koc <alexander.koc@slu.se>
2019-06-17 11:56:19 +0200

    Move spectrawiz-specific functions to read_spectrawiz.R and temporarily remove official multiple file reading support.

--- R/read-spectrawiz.R
+++ R/read-spectrawiz.R
@@ -1,4 +1,4 @@
-#' @include process-spectra.R
+#' @include util.R
 
 #' @name read_spectrawiz
 #' @title Read one or more SpectraWiz output files into a dataframe
@@ -27,7 +27,7 @@
 #' print(spectral_data[1,1:5])
 #'
 #' @export
-read_spectrawiz <- function(file, different_spectral_ranges=FALSE) {
+read_spectrawiz <- function(file) {
 
   # Throw error if one or more files are missing at specified path
   stop_if_files_are_missing(file)
@@ -38,8 +38,38 @@ read_spectrawiz <- function(file, different_spectral_ranges=FALSE) {
   }
 
   # Filepaths to data frames:
-  spectral_data <- lapply(file, process_spectra)
-  spectral_data <- merge_spectra(spectral_data, different_spectral_ranges)
+  spectral_data <- lapply(file, process_spectrawiz_spectra)
+  spectral_data <- merge_spectra(spectral_data, FALSE)
   spectral_data
 }
 
+# Internal function for processing a single spectrawiz file
+process_spectrawiz_spectra <- function(file) {
+  file_contents <- scan(file, what = character(), sep = "\n",
+                        quiet = TRUE, comment.char = "\"")
+
+  # Remove whitespace from all lines:
+  file_contents <- trimws(file_contents)
+
+  # Split by a single space and remove any empty columns per row
+  # TODO: Investigate if this is needed or if spectrawiz *always* uses the [...]
+  # ... same column delim spacing
+  file_contents <- strsplit(file_contents, split = " ")
+  file_contents <- lapply(file_contents, function(entry) entry[!entry == ""])
+
+  # Combine all wavelength rows into a matrix, transpose into wide form and
+  # convert into a data frame
+  file_contents <- do.call(rbind, file_contents)
+
+  # Wavelengths are converted to char to be used as column names
+  wavelengths <- as.character(file_contents[, 1])
+  spectral_data <- t(as.double(file_contents[, 2]))
+  colnames(spectral_data) <- wavelengths
+  spectral_data <- as.data.frame(spectral_data)
+
+  # Add filename of loaded file as a column
+  # TODO: Make filename column optional?
+  spectral_data <- cbind(data.frame(filename = file, stringsAsFactors = FALSE),
+                         spectral_data)
+  spectral_data
+}
